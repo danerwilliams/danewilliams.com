b88c193394d401cc3999812f45b906c0809c787f
Dane Williams <dane_williams@outlook.com>
2022-10-01 20:02:34 -0400

    Create boilerplate for pages (#14)

---
+++ pages/about.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const About: NextPage = () => {
+  return <h1 className="text-3xl font-bold underline">About</h1>;
+};
+
+export default About;
---
+++ pages/journal.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Journal: NextPage = () => {
+  return <h1 className="text-3xl font-bold underline">Journal</h1>;
+};
+
+export default Journal;
