35a6a95fbf8b79c60dbe580ef2dce8611d212bb0
Dane Williams <dane_williams@outlook.com>
2023-01-01 18:27:44 -0500

    Animate desktop nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,49 +1,39 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 interface NavbarProps {}
 
-export const Navbar: FC<Readonly<NavbarProps>> = () => {
-  return (
-    <NavigationMenu.Root className="flex text-lightmode-slate11 dark:text-darkmode-slate11">
-      <NavigationMenu.List className="flex gap-32">
-        <NavigationMenu.Item>
-          <NavigationMenu.Link
-            className="NavigationMenuLink"
-            href="https://github.com/radix-ui"
-          >
-            About
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
-        <NavigationMenu.Item>
-          <NavigationMenu.Link
-            className="NavigationMenuLink"
-            href="https://github.com/radix-ui"
-          >
-            Journal
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
-        <NavigationMenu.Item>
-          <NavigationMenu.Link
-            className="NavigationMenuLink"
-            href="https://github.com/radix-ui"
-          >
-            Projects
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
-        <NavigationMenu.Item>
-          <NavigationMenu.Link
-            className="NavigationMenuLink"
-            href="https://github.com/radix-ui"
-          >
-            Gallery
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
-      </NavigationMenu.List>
+const pages = ['about', 'journal', 'projects', 'gallery'];
 
-      <div className="ViewportPosition">
-        <NavigationMenu.Viewport className="NavigationMenuViewport" />
-      </div>
+export const Navbar: FC<Readonly<NavbarProps>> = () => {
+  const [hovered, setHovered] = useState<string | undefined>();
+
+  return (
+    <NavigationMenu.Root className="flex mt-6 text-lightmode-slate11 dark:text-darkmode-slate11">
+      <NavigationMenu.List className="flex gap-32">
+        {pages.map((page) => {
+          const isHovered = hovered === page;
+
+          return (
+            <motion.div
+              onHoverStart={() => setHovered(page)}
+              onHoverEnd={() => setHovered(undefined)}
+              animate={{ y: isHovered ? -4 : 0 }}
+              key={page}
+            >
+              <Link href={`/${page}`}>
+                <a>
+                  <NavigationMenu.Item className="py-4 px-2">
+                    {`${page.charAt(0).toUpperCase()}${page.slice(1)}`}
+                  </NavigationMenu.Item>
+                </a>
+              </Link>
+            </motion.div>
+          );
+        })}
+      </NavigationMenu.List>
     </NavigationMenu.Root>
   );
 };
