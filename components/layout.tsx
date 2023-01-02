5e48a2e23fd1d594ca129e6547fe368119538520
Dane Williams <dane_williams@outlook.com>
2023-01-02 15:14:33 -0800

    Create `Navbar` (#16)

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from 'react';
 import { Footer } from './footer';
+import { Navbar } from './navbar';
 
 interface LayoutProps {
   children: ReactNode;
@@ -7,11 +8,9 @@ interface LayoutProps {
 
 export const Layout: FC<Readonly<LayoutProps>> = ({ children }) => {
   return (
-    <div className="flex flex-col items-center bg-lightmode-slate1 dark:bg-darkmode-slate1">
-      {/* TODO: Create Header */}
-      <div className="min-h-screen">
-        <main>{children}</main>
-      </div>
+    <div className="flex flex-col min-h-screen items-center bg-lightmode-slate1 dark:bg-darkmode-slate1">
+      <Navbar />
+      <main className="grow">{children}</main>
       <Footer />
     </div>
   );
--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,7 +1,24 @@
 import type { NextPage } from 'next';
 
 const Gallery: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Gallery</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Gallery</h1>
+      <a>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industrys standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum. Why do we use it? It is a long established fact that a
+        reader will be distracted by the readable content of a page when looking
+        at its layout. The point of using Lorem Ipsum is that it has a
+      </a>
+    </>
+  );
 };
 
 export default Gallery;
