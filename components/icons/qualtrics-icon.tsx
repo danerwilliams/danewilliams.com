e7fb1062e85f2359ba1cae06067e13fc99833e9e
Dane Williams <dane_williams@outlook.com>
2023-01-10 21:13:54 -0500

    Fix Symetra logo bug (#33)

--- components/icons/qualtrics-icon.tsx
+++ components/icons/qualtrics-icon.tsx
@@ -12,6 +12,7 @@ export const QualtricsIcon: FC<Readonly<QualtricsIconProps>> = ({
     width="100%"
     height="100%"
     viewBox="0 0 360 200"
+    fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
