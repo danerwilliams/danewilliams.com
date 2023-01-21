cde4aa670797298d308bb28b6bb13bc6497c9dfb
Dane Williams <dane_williams@outlook.com>
2023-01-21 15:57:40 -0500

    wip

--- pages/journal.tsx
+++ pages/journal.tsx
@@ -1,7 +1,38 @@
 import type { NextPage } from 'next';
+import { Article, ArticleProps } from '../components/article';
+import { Page } from '../components/page';
+import { PageHeader } from '../components/page-header';
+
+const articles: ArticleProps[] = [
+  {
+    title: 'Officially a Notre Dame alumnus',
+    preview:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+    date: new Date(),
+    url: '/journal/officially-a-notre-dame-alumnus',
+  },
+];
 
 const Journal: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Journal</h1>;
+  return (
+    <Page>
+      <PageHeader question="What do I think?" />
+      <div className="mt-8">
+        <p>
+          Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Sit amet dictum sit amet
+          justo.
+        </p>
+        <div className="mt-8">
+          {articles.map((article) => (
+            <div key={article.title}>
+              <Article {...article} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </Page>
+  );
 };
 
 export default Journal;
