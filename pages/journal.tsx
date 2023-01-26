51b4dc551c33325b20592e0ae8128621ec6b02b7
Dane Williams <dane_williams@outlook.com>
2023-01-25 19:18:56 -0500

    wip

--- pages/journal.tsx
+++ pages/journal.tsx
@@ -3,12 +3,35 @@ import { Article } from '../components/article';
 import { getAllArticles, Article as ArticleMetadata } from '../lib/journal';
 import { Page } from '../components/page';
 import { PageHeader } from '../components/page-header';
+import Fuse from 'fuse.js';
+import { useState } from 'react';
 
 interface JournalProps {
   articles: ArticleMetadata[];
 }
 
 const Journal: NextPage<JournalProps> = ({ articles }) => {
+  const [searchArticles, setSearchArticles] = useState(articles);
+
+  const fuse = new Fuse(searchArticles, {
+    keys: ['name', 'tags'],
+  });
+
+  const search = (query: string) => {
+    if (!query) {
+      setSearchArticles(articles);
+      return;
+    }
+    query = query.toLowerCase();
+
+    const fuse = new Fuse(articles, {
+      keys: ['title', 'description'],
+    });
+    const result = fuse.search(query);
+
+    setSearchArticles(result.map((r) => r.item));
+  };
+
   return (
     <Page>
       <PageHeader question="What do I think?" />
@@ -18,6 +41,11 @@ const Journal: NextPage<JournalProps> = ({ articles }) => {
           incididunt ut labore et dolore magna aliqua. Sit amet dictum sit amet
           justo.
         </p>
+        <input
+          type="search"
+          onChange={(e) => search(e.target.value)}
+          placeholder="Search Technologies"
+        />
         <div className="mt-8">
           {articles.map((article) => (
             <div className="mt-4" key={article.title}>
