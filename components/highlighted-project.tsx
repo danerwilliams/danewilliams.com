dc63647c680add694a15c5eb3e8e42ee887fe931
Dane Williams <dane_williams@outlook.com>
2023-03-20 21:51:20 -0400

    Creates highlighted projects

---
+++ components/highlighted-project.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link';
+import { FC, ReactNode } from 'react';
+
+interface Stat {
+  icon: ReactNode;
+  label: string;
+}
+
+export interface HighlightedProjectProps {
+  name: string;
+  url: string;
+  description: string;
+  stats: [Stat, Stat];
+}
+
+export const HighlightedProject: FC<Readonly<HighlightedProjectProps>> = ({
+  name,
+  url,
+  description,
+  stats,
+}) => {
+  return (
+    <a href={url} target="_blank" className="flex grow" rel="noreferrer">
+      <div className="flex grow h-40 border rounded-xl border-lightmode-border dark:border-darkmode-border bg-lightmode-component dark:bg-darkmode-component hover:bg-lightmode-component-hover hover:dark:bg-darkmode-component-hover duration-200">
+        <div className="flex grow flex-col items-center justify-between m-3">
+          <h2 className="text-2xl font-medium text-lightmode-text-high-contrast dark:text-darkmode-text-high-contrast">
+            {name}
+          </h2>
+          <p className="text-center">{description}</p>
+          <div className="flex w-full justify-around">
+            {stats.map((stat) => (
+              <div
+                className="flex items-center gap-1 font-light lg:text-base text-sm"
+                key={stat.label}
+              >
+                {stat.icon}
+                {stat.label}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </a>
+  );
+};
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,7 +1,51 @@
+import { EyeOpenIcon, PersonIcon, StarIcon } from '@radix-ui/react-icons';
 import type { NextPage } from 'next';
+import { HighlightedProject } from '../components/highlighted-project';
+import { Page } from '../components/page';
+import { PageHeader } from '../components/page-header';
+import { SectionHeader } from '../components/section-header';
 
 const Projects: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Projects</h1>;
+  return (
+    <Page>
+      <PageHeader question="What have I built?" />
+      <div className="mt-8">
+        <p>
+          Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Sit amet dictum sit amet
+          justo.
+        </p>
+        <div className="flex flex-row max-[576px]:flex-col gap-6 mt-8">
+          <div className="flex w-full">
+            <HighlightedProject
+              name="New Grad Positions"
+              url="https://github.com/coderQuad/New-Grad-Positions-2023"
+              description="A collection of computer science jobs for new college graduates"
+              stats={[
+                { icon: <StarIcon />, label: '5k+ Stars' },
+                { icon: <EyeOpenIcon />, label: '10k+ Visits/Day' },
+              ]}
+            />
+          </div>
+          <div className="flex w-full">
+            <HighlightedProject
+              name="Dracula Tmux"
+              url="https://github.com/dracula/tmux"
+              description="Official Dracula Theme extension for Tmux"
+              stats={[
+                { icon: <StarIcon />, label: '400+ Stars' },
+                { icon: <PersonIcon />, label: '20+ Contributors' },
+              ]}
+            />
+          </div>
+        </div>
+        <div className="mt-8">
+          <SectionHeader text="Timeline" />
+          <div className="mt-8"></div>
+        </div>
+      </div>
+    </Page>
+  );
 };
 
 export default Projects;
