577a6c00bd3bc3b7f2bb7d314402617587d4d9f4
Dane Williams <dane_williams@outlook.com>
2023-03-27 20:10:15 -0400

    Modify gallery photos (#51)

--- content/gallery/images.ts
+++ content/gallery/images.ts
@@ -2,125 +2,19 @@ import { GalleryImageProps } from '../../components/gallery-image';
 
 export const GalleryImages: Omit<GalleryImageProps, 'onClick'>[] = [
   {
-    src: '/static/gallery/bern-bear.jpeg',
-    height: 360,
-    caption: 'Bern, CH',
+    src: '/static/gallery/milk-jugs.jpeg',
+    height: 220,
+    caption: 'Newport Beach, CA',
   },
   {
-    src: '/static/gallery/the-bean.jpeg',
-    height: 200,
-    caption: 'Chicago, IL',
-  },
-  {
-    src: '/static/gallery/lennon-wall.jpeg',
-    height: 300,
-    caption: 'Prague, CZ',
-  },
-  {
-    src: '/static/gallery/london-pub.jpeg',
-    height: 260,
-    caption: 'London, UK',
-  },
-  {
-    src: '/static/gallery/vancouver.jpeg',
-    height: 200,
-    caption: 'Vancouver, CA',
-  },
-  {
-    src: '/static/gallery/coders-grad.jpeg',
-    height: 240,
-    caption: 'Notre Dame, IN',
-  },
-  {
-    src: '/static/gallery/yosemite-pano.jpeg',
-    height: 100,
-    caption: 'Yosemite, CA',
-  },
-  {
-    src: '/static/gallery/compound-roof.jpeg',
-    height: 360,
-    caption: 'South Bend, IN',
-  },
-  {
-    src: '/static/gallery/fisher-2022-grad-class.jpeg',
+    src: '/static/gallery/sf-steps.jpeg',
     height: 250,
-    caption: 'Notre Dame, IN',
+    caption: 'San Francisco, CA',
   },
   {
-    src: '/static/gallery/florence-dome.jpeg',
-    height: 300,
-    caption: 'Florence, IT',
-  },
-  {
-    src: '/static/gallery/franz-prague.jpeg',
-    height: 300,
-    caption: 'Prague, CZ',
-  },
-  {
-    src: '/static/gallery/gateway-to-the-east.jpeg',
-    height: 280,
-    caption: 'St. Louis, MO',
-  },
-  {
-    src: '/static/gallery/cinque-terre-baggies.jpeg',
-    height: 280,
-    caption: 'Cinque Terre, IT',
-  },
-  {
-    src: '/static/gallery/love-creek.jpeg',
-    height: 300,
-    caption: 'Niles, MI',
-  },
-  {
-    src: '/static/gallery/mariners-game.jpeg',
-    height: 380,
-    caption: 'Seattle, WA',
-  },
-  {
-    src: '/static/gallery/mega-table.jpeg',
+    src: '/static/gallery/muir-beach.jpeg',
     height: 190,
-    caption: 'Notre Dame, IN',
-  },
-  {
-    src: '/static/gallery/nellie-portrait.jpeg',
-    height: 300,
-    caption: 'Pleasanton, CA',
-  },
-  {
-    src: '/static/gallery/pittsburgh-batman.jpeg',
-    height: 300,
-    caption: 'Pittsburgh, PA',
-  },
-  {
-    src: '/static/gallery/alumni-tourney.jpeg',
-    caption: 'Notre Dame, IN',
-    height: 140,
-  },
-  {
-    src: '/static/gallery/tulum-heart.jpeg',
-    height: 240,
-    caption: 'Tulum, MX',
-  },
-  {
-    src: '/static/gallery/white-sox.jpeg',
-    height: 140,
-    caption: 'Chicago, IL',
-  },
-
-  {
-    src: '/static/gallery/amazon-spheres.jpeg',
-    height: 200,
-    caption: 'Seattle, WA',
-  },
-  {
-    src: '/static/gallery/parker-house.jpeg',
-    height: 150,
-    caption: 'Sea Girt, NJ',
-  },
-  {
-    src: '/static/gallery/the-yard-sunset.jpeg',
-    height: 250,
-    caption: 'New York, NY',
+    caption: 'Muir Beach, CA',
   },
   {
     src: '/static/gallery/beacon-hike.jpeg',
@@ -128,24 +22,14 @@ export const GalleryImages: Omit<GalleryImageProps, 'onClick'>[] = [
     caption: 'Beacon, NY',
   },
   {
-    src: '/static/gallery/cactus.jpeg',
-    height: 340,
-    caption: 'Tucson, AZ',
+    src: '/static/gallery/the-yard-sunset.jpeg',
+    height: 250,
+    caption: 'New York, NY',
   },
   {
-    src: '/static/gallery/florence-dome-selfie.jpeg',
-    height: 270,
-    caption: 'Florence, IT',
-  },
-  {
-    src: '/static/gallery/hot-dogs-plus.jpeg',
-    height: 260,
-    caption: 'Niles, MI',
-  },
-  {
-    src: '/static/gallery/interlaken.jpeg',
-    height: 220,
-    caption: 'Interlaken, CH',
+    src: '/static/gallery/vancouver.jpeg',
+    height: 200,
+    caption: 'Vancouver, CA',
   },
   {
     src: '/static/gallery/mcsorleys.jpeg',
@@ -158,13 +42,143 @@ export const GalleryImages: Omit<GalleryImageProps, 'onClick'>[] = [
     caption: 'Chicago, IL',
   },
   {
-    src: '/static/gallery/pool-dogs.jpeg',
+    src: '/static/gallery/cactus.jpeg',
+    height: 340,
+    caption: 'Tucson, AZ',
+  },
+  {
+    src: '/static/gallery/florence-dome.jpeg',
     height: 300,
-    caption: 'Notre Dame, IN',
+    caption: 'Florence, IT',
+  },
+  {
+    src: '/static/gallery/florence-dome-selfie.jpeg',
+    height: 270,
+    caption: 'Florence, IT',
+  },
+  {
+    src: '/static/gallery/cinque-terre-baggies.jpeg',
+    height: 280,
+    caption: 'Cinque Terre, IT',
+  },
+  {
+    src: '/static/gallery/bern-bear.jpeg',
+    height: 360,
+    caption: 'Bern, CH',
+  },
+  {
+    src: '/static/gallery/interlaken.jpeg',
+    height: 220,
+    caption: 'Interlaken, CH',
+  },
+  {
+    src: '/static/gallery/lennon-wall.jpeg',
+    height: 300,
+    caption: 'Prague, CZ',
+  },
+  {
+    src: '/static/gallery/franz-prague.jpeg',
+    height: 300,
+    caption: 'Prague, CZ',
+  },
+  {
+    src: '/static/gallery/london-pub.jpeg',
+    height: 260,
+    caption: 'London, UK',
+  },
+  {
+    src: '/static/gallery/parker-house.jpeg',
+    height: 150,
+    caption: 'Sea Girt, NJ',
   },
   {
     src: '/static/gallery/san-onofre.jpeg',
     height: 300,
     caption: 'San Onofre, CA',
+  },
+  {
+    src: '/static/gallery/coders-grad.jpeg',
+    height: 240,
+    caption: 'Notre Dame, IN',
+  },
+  {
+    src: '/static/gallery/fisher-2022-grad-class.jpeg',
+    height: 250,
+    caption: 'Notre Dame, IN',
+  },
+  {
+    src: '/static/gallery/white-sox.jpeg',
+    height: 140,
+    caption: 'Chicago, IL',
+  },
+  {
+    src: '/static/gallery/tulum-heart.jpeg',
+    height: 240,
+    caption: 'Tulum, MX',
+  },
+  {
+    src: '/static/gallery/compound-roof.jpeg',
+    height: 360,
+    caption: 'South Bend, IN',
+  },
+  {
+    src: '/static/gallery/pool-dogs.jpeg',
+    height: 300,
+    caption: 'Notre Dame, IN',
+  },
+  {
+    src: '/static/gallery/alumni-tourney.jpeg',
+    caption: 'Notre Dame, IN',
+    height: 140,
+  },
+  {
+    src: '/static/gallery/the-bean.jpeg',
+    height: 200,
+    caption: 'Chicago, IL',
+  },
+  {
+    src: '/static/gallery/hot-dogs-plus.jpeg',
+    height: 260,
+    caption: 'Niles, MI',
+  },
+  {
+    src: '/static/gallery/gateway-to-the-east.jpeg',
+    height: 280,
+    caption: 'St. Louis, MO',
+  },
+  {
+    src: '/static/gallery/mariners-game.jpeg',
+    height: 380,
+    caption: 'Seattle, WA',
+  },
+  {
+    src: '/static/gallery/yosemite-pano.jpeg',
+    height: 100,
+    caption: 'Yosemite, CA',
+  },
+  {
+    src: '/static/gallery/love-creek.jpeg',
+    height: 300,
+    caption: 'Niles, MI',
+  },
+  {
+    src: '/static/gallery/nellie-portrait.jpeg',
+    height: 300,
+    caption: 'Pleasanton, CA',
+  },
+  {
+    src: '/static/gallery/mega-table.jpeg',
+    height: 190,
+    caption: 'Notre Dame, IN',
+  },
+  {
+    src: '/static/gallery/pittsburgh-batman.jpeg',
+    height: 300,
+    caption: 'Pittsburgh, PA',
+  },
+  {
+    src: '/static/gallery/amazon-spheres.jpeg',
+    height: 200,
+    caption: 'Seattle, WA',
   },
 ];
